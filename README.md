b0777d5cd1ac5b7e9b938c1133bbf09625746026
Alex Costinescu <paul.costinescu@gmail.com>
2020-02-09 12:06:54 -0700

    Code cleanup + commenting. README updated.

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # One-Button TicTacToe
 **Alex Costinescu**
 
+A simple TicTacToe game built using JQuery that is played using only one button. Built for an Input Interaction and Accessibility course at CU Boulder and designed for players that have limited mobility.
+
 <br />
 
 ## System Requirements
-A computer or device with javascript support and a keyboard attached.
+A computer or device with JavaScript support and a keyboard attached. Does not currently support touch devices.
+
+<br />
 
 ## Instructions
-1. Open index.html ( OR: https://acostinescu.github.io/One-Button-TicTacToe/ ) in the browser of your choice.
+1. Open index.html ( Also available here: https://acostinescu.github.io/One-Button-TicTacToe/ ) in the browser of your choice.
 2. Follow the instructions on screen and play!
+
+<br />
+
+## Slightly Longer Instructions
+This game is played used just the spacebar. 
+
+By tapping the spacebar, players  cycle through rows on the board. Once a desired row is selected, pressing and holding the spacebar enters that row and switches the game into column selection mode. 
+
+Once in column selection mode, tapping the spacebar cycles through columns in the selected row. Pressing and holding in this mode marks a column with either an X or an O and advances the turn. 
+
+Players can also tap the spacebar twice in quick succession to exit column selection mode and return to row selection mode without marking a square.
+
+Once a player has won, or the game ends in a tie, a dialog will appear announcing the winner. Tapping space now clears the dialog and resets the board for the next game.
--- game.js
+++ game.js
@@ -1,26 +1,82 @@
-const rowList = $(".gameboard_row");
-const holdTime = 700;
-const doublePressTime = 175;
+/**
+ * Board Elements
+ */
+const ROW_LIST = $(".gameboard_row");
+const SQUARE_LIST = $(".gameboard_square");
+const LEFT_RIGHT_DIAG = SQUARE_LIST.eq(0).add(SQUARE_LIST.eq(4)).add(SQUARE_LIST.eq(8));
+const RIGHT_LEFT_DIAG = SQUARE_LIST.eq(2).add(SQUARE_LIST.eq(4)).add(SQUARE_LIST.eq(6));
+const TURN_BOX = $("#turn_box");
+const TURN_BOX_ICON = $("#turn_box_icon");
+const TURN_BOX_TEXT = $("#turn_box_text");
+
+
+/**
+ * Character for the keyboard key that is being used as the game key
+ */
 const KEYBOARD_BUTTON = " ";
 
+
+/**
+ * Selection modes
+ * selectionMode - Current selection mode
+ * prevSelectionMode - Used for alerts, used to return to previous mode when alert is closed
+ * SMODE_ROW - Row selection mode
+ * SMODE_COL - Column selection mode
+ * SMODE_ALERT - Alert interaction mode
+ */
 var selectionMode = 0;
+var prevSelectionMode;
 const SMODE_ROW = 0;
 const SMODE_COL = 1;
 const SMODE_ALERT = 2;
 
+
+/**
+ * Key hold
+ * keyDownFlag - Tracks if the game key is currently being held down
+ * keyHoldTimeout - Timeout for tracking the keyhold event
+ * HOLD_TIME - Time in ms that the game key needs to be held down to trigger the keyhold event
+ */
 var keyDownFlag = false;
-var keyHoldFunc;
-var keyRepeatFunc;
+var keyHoldTimeout;
+const HOLD_TIME = 700;
+
+
+/**
+ * Key double-tap
+ * keyRepeatFlag - Tracks if we are waiting for a second press to trigger the double-tap event
+ * keyRepeatFlagRemove - Tracks if we need to stop waiting for a second press
+ * keyRepeatTimeout - Timeout for tracking the double-tap event
+ * DOUBLE_PRESS_TIME - Max time in ms between taps to trigger a double-tap event
+ */
 var keyRepeatFlag = false;
 var keyRepeatFlagRemove = false;
+var keyRepeatTimeout;
+const DOUBLE_PRESS_TIME = 175;
 
+
+/**
+ * Tracks whether to block certain keyup events
+ */
 var blockKeyupFlag = false;
 
+
+/**
+ * Players
+ * currentPlayer - The player whose turn it currently is
+ * PLAYER_ONE - The first player (x)
+ * PLAYER_TWO - The second player (o)
+ * TIE_GAME - Used when checking for a win state if no one has won and there are no empty squares left
+ */
 var currentPlayer = 0;
 const PLAYER_ONE = 0;
 const PLAYER_TWO = 1;
 const TIE_GAME = -1;
 
+
+/**
+ * Keypress (keydown) function
+ */
 $(document).keypress(function(e){
     if(e.key == KEYBOARD_BUTTON && !keyDownFlag){
         
@@ -28,27 +84,27 @@ $(document).keypress(function(e){
         if(selectionMode == SMODE_ROW)
         {
             // Check for holding key
-            keyHoldFunc = setTimeout(function(){
+            keyHoldTimeout = setTimeout(function(){
                 blockKeyupFlag = true;
                 selectRow();
                 highlightNextCol();
-            }, holdTime)
+            }, HOLD_TIME)
         }
 
         // Column selection mode (row is selected)
         else if (selectionMode == SMODE_COL){
 
-            keyHoldFunc = setTimeout(function(){
+            keyHoldTimeout = setTimeout(function(){
                 markCol();
-            }, holdTime);
+            }, HOLD_TIME);
 
             // Check for double pressing key
             if(!keyRepeatFlag){
                 keyRepeatFlag = true;
-                keyRepeatFunc = setTimeout(function(){
+                keyRepeatTimeout = setTimeout(function(){
                     keyRepeatFlag = false;
                     keyRepeatFlagRemove = true;
-                }, doublePressTime);
+                }, DOUBLE_PRESS_TIME);
             }
             else {
                 // Handle double press
@@ -67,11 +123,15 @@ $(document).keypress(function(e){
     keyDownFlag = true;
 });
 
+
+/**
+ * Keyup function
+ */
 $(document).on("keyup", function(e){
     if(e.key == KEYBOARD_BUTTON){
         
         // When the key is lifted, clear the timeout function that handles the key hold event
-        clearTimeout(keyHoldFunc);
+        clearTimeout(keyHoldTimeout);
         
         if(selectionMode == SMODE_ROW) {
             if(!blockKeyupFlag) highlightNextRow();
@@ -86,21 +146,33 @@ $(document).on("keyup", function(e){
 });
 
 
-
+/**
+ * Highlight the next row, cycles back to the first row if the last row is selected
+ */
 function highlightNextRow(){
-    var currentRow = rowList.filter(".highlighted");
-    rowList.removeClass("highlighted");
+    var currentRow = ROW_LIST.filter(".highlighted");
+    ROW_LIST.removeClass("highlighted");
 
     var nextRow;
-    if(!currentRow.length || currentRow.is(":last-child")) nextRow = rowList.eq(0);
+    if(!currentRow.length || currentRow.is(":last-child")) nextRow = ROW_LIST.eq(0);
     else nextRow = currentRow.next();
     nextRow.addClass("highlighted");
 }
+
+
+/**
+ * Clear the highlighted row
+ */
 function clearHighlightedRow(){
-    rowList.removeClass("highlighted");
+    ROW_LIST.removeClass("highlighted");
 }
+
+
+/**
+ * Select the currently highlighted row
+ */
 function selectRow(){
-    var currentRow = rowList.filter(".highlighted");
+    var currentRow = ROW_LIST.filter(".highlighted");
     if(currentRow.length) {
         currentRow.addClass("selected");
         selectionMode = SMODE_COL;
@@ -109,13 +181,22 @@ function selectRow(){
         console.error("Error: No row was highlighted when attempting to make a selection!");
     }
 }
+
+
+/**
+ * Clear the selected row
+ */
 function clearSelectedRow(){
-    rowList.removeClass("selected");
+    ROW_LIST.removeClass("selected");
     selectionMode = SMODE_ROW;
 } 
 
+
+/**
+ * Highlight the next column, cycles back to the first column if the last on if currently selected
+ */
 function highlightNextCol(){
-    var currentRow = rowList.filter(".selected");
+    var currentRow = ROW_LIST.filter(".selected");
     if(currentRow.length){
         var colList = currentRow.children(".gameboard_square");
         var currentCol = colList.filter(".highlighted");
@@ -130,10 +211,20 @@ function highlightNextCol(){
         console.error("Error: No row was selected when attempting to highlight a column!");
     }
 }
+
+
+/**
+ * Clear the highlighted column
+ */
 function clearHighlightedCol(){
     var colList = $(".gameboard_square");
     colList.removeClass("highlighted");
 }
+
+
+/**
+ * Mark a column (box) with an x or o
+ */
 function markCol(){
     var currentCol = $(".gameboard_square.highlighted");
     if(currentCol.length){
@@ -169,9 +260,11 @@ function markCol(){
     }
 }
 
-const turnBox = $("#turn_box");
-const turnBoxIcon = $("#turn_box_icon");
-const turnBoxText = $("#turn_box_text");
+
+/**
+ * Advance the turn to the next player (or the designated player)
+ * @param {Number} setTurn the player to set the turn to (optional)
+ */
 function advanceTurn(setTurn){
     if(setTurn == null) {
         if(currentPlayer == PLAYER_ONE) currentPlayer = PLAYER_TWO;
@@ -180,93 +273,115 @@ function advanceTurn(setTurn){
     else currentPlayer = setTurn;
 
     if(currentPlayer == PLAYER_ONE) {
-        turnBox.removeClass("p2").addClass("p1");
-        turnBoxIcon.attr("src", "images/x_sq.svg");
-        turnBoxText.html("Player <strong>One's</strong> Turn");
+        TURN_BOX.removeClass("p2").addClass("p1");
+        TURN_BOX_ICON.attr("src", "images/x_sq.svg");
+        TURN_BOX_TEXT.html("Player <strong>One's</strong> Turn");
     }
     else {
-        turnBox.removeClass("p1").addClass("p2");
-        turnBoxIcon.attr("src", "images/o.svg");
-        turnBoxText.html("Player <strong>Two's</strong> Turn");
+        TURN_BOX.removeClass("p1").addClass("p2");
+        TURN_BOX_ICON.attr("src", "images/o.svg");
+        TURN_BOX_TEXT.html("Player <strong>Two's</strong> Turn");
     }
 }
 
-const squareList = $(".gameboard_square");
-const leftRightDiag = squareList.eq(0).add(squareList.eq(4)).add(squareList.eq(8));
-const rightLeftDiag = squareList.eq(2).add(squareList.eq(4)).add(squareList.eq(6));
-const restartMessage = "<strong>Tap the spacebar or click 'Okay' to play again!</strong>";
+
+/**
+ * Check the current state of the board to see if there is a winner
+ */
+const RESTART_MESSAGE = "<strong>Tap the spacebar or click 'Okay' to play again!</strong>";
 function checkState(){
     var winner;
 
     // Row Winner
     for(var row = 0; row <= 6; row += 3){
-        var currRow = squareList.slice(row, row + 3);
+        var currRow = SQUARE_LIST.slice(row, row + 3);
         if(currRow.filter(".square_x").length == 3) winner = PLAYER_ONE;
         else if (currRow.filter(".square_o").length == 3) winner = PLAYER_TWO;
     }
 
     // Column Winner
     for(var col = 0; col < 3; col++){
-        var currCol = squareList.eq(col).add(squareList.eq(col + 3)).add(squareList.eq(col + 6));
+        var currCol = SQUARE_LIST.eq(col).add(SQUARE_LIST.eq(col + 3)).add(SQUARE_LIST.eq(col + 6));
         if(currCol.filter(".square_x").length == 3) winner = PLAYER_ONE;
         else if (currCol.filter(".square_o").length == 3) winner = PLAYER_TWO;
     }
 
     // Left to Right Diagonal Winner
-    if(leftRightDiag.filter(".square_x").length == 3) winner = PLAYER_ONE;
-    else if (leftRightDiag.filter(".square_o").length == 3) winner = PLAYER_TWO;
+    if(LEFT_RIGHT_DIAG.filter(".square_x").length == 3) winner = PLAYER_ONE;
+    else if (LEFT_RIGHT_DIAG.filter(".square_o").length == 3) winner = PLAYER_TWO;
 
     // Right to Left Diagonal Winner
-    if (rightLeftDiag.filter(".square_x").length == 3) winner = PLAYER_ONE;
-    else if (rightLeftDiag.filter(".square_o").length == 3) winner = PLAYER_TWO;
+    if (RIGHT_LEFT_DIAG.filter(".square_x").length == 3) winner = PLAYER_ONE;
+    else if (RIGHT_LEFT_DIAG.filter(".square_o").length == 3) winner = PLAYER_TWO;
 
     // Board is full and no previous winner - tied game
-    if (squareList.filter(".marked").length == 9 && winner == null) winner = TIE_GAME;
+    if (SQUARE_LIST.filter(".marked").length == 9 && winner == null) winner = TIE_GAME;
 
     // If we have a game ending state, show the winner or tie alert then clear the board
     if (winner == PLAYER_ONE){
-        createAlert ("Player One Wins!", restartMessage, function(){clearBoard()});
+        createAlert ("Player One Wins!", RESTART_MESSAGE, function(){clearBoard()});
     }
     else if (winner == PLAYER_TWO){
-        createAlert("Player Two Wins!", restartMessage, function(){clearBoard()});
+        createAlert("Player Two Wins!", RESTART_MESSAGE, function(){clearBoard()});
     }
     else if(winner == TIE_GAME){
-        createAlert("Tied Game!", restartMessage, function(){clearBoard()});
+        createAlert("Tied Game!", RESTART_MESSAGE, function(){clearBoard()});
     }
 
     return winner;
 }
 
-// Clear the board, reset the board to player one, and set the selection more to row mode
+
+/**
+ * Clear the board, reset the board to player one, and set the selection more to row mode
+ */
 function clearBoard(){
-    squareList.removeClass("marked").removeClass("square_x").removeClass("square_o");   
+    SQUARE_LIST.removeClass("marked").removeClass("square_x").removeClass("square_o");   
     advanceTurn(0);
     selectionMode = SMODE_ROW;
 }
 
-var prevSelectionMode; // Track whatever mode we were in before the alert was made
 
-// Create an alert with title, text, and an optional callback
+/**  
+ * Create an alert with title, text, and an optional callback function
+ */
 function createAlert(title, text, callback){
+
+    // Save previous selection mode and switch to SMODE_ALERT
     prevSelectionMode = selectionMode;
     selectionMode = SMODE_ALERT;
+
+    // Alert background
     var background = $("<div class='alert_bg'></div>");
+
+    // Alert content
     var alertBox = $("<div class='alert_box'></div>").appendTo(background);
-    var alertTitle = $("<h2 class='alert_title'></h2>").html(title).appendTo(alertBox);
-    var alertText = $("<p class='alert_text'></p>").html(text).appendTo(alertBox);
+    $("<h2 class='alert_title'></h2>").html(title).appendTo(alertBox);
+    $("<p class='alert_text'></p>").html(text).appendTo(alertBox);
+
+    // Alert 'Okay' button
     var alertButtonBox = $("<div class='alert_btnbox'></div>").appendTo(alertBox);
-    var alertButton = $("<button class='alert_btn'>Okay!</button>").on("click", function(){
+    $("<button class='alert_btn'>Okay!</button>").on("click", function(){
         closeAlert(callback);
     }).appendTo(alertButtonBox);
+
     background.appendTo($("body"));
 }
-// Close any existing alert, revert the selection mode, and call the optional callback function
+
+
+/**
+ *  Close any existing alert, revert the selection mode, and call the optional callback function
+ */ 
 function closeAlert(callback){
     selectionMode = prevSelectionMode;
     $(".alert_bg").remove();
     if(callback) callback();
 }
 
+
+/**
+ * Create a help alert which explains how to play the game
+ */
 function createHelpAlert(){
     var helpTitle = "Welcome to One-Button TicTacToe!";
     var helpText = "<strong>This game is played using the spacebar:</strong>";
@@ -279,21 +394,32 @@ function createHelpAlert(){
     createAlert(helpTitle, helpText);
 }
 
+
+/**
+ * Automatically show the help alert the first time someone opens the page
+ */
 $(document).ready(function(){
-    // Only show the help dialog the first time the page is loaded
-    if(document.cookie != "helpshown=true")
-    {
+    if(document.cookie != "helpshown=true"){
         document.cookie = "helpshown=true"
         createHelpAlert();
     }
 });
 
+
+/**
+ * Clear the game and restart when the restart button is pressed
+ */
 $("#aRestart").click(function(){
     clearBoard();
     clearHighlightedCol();
     clearHighlightedRow();
     clearSelectedRow();
 })
+
+
+/**
+ * Create the help alert when the help button is pressed
+ */
 $("#aHelp").click(function(){
     createHelpAlert();
 });
